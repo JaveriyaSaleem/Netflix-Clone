12ce26e26661ca9fc483904cfad40f8e7991cfb8
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2024-11-07 13:01:48 +0500

    added

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
-let previousBtn = document.getElementById('previousBtn')
-let nextBtn = document.getElementById('nextBtn')
-
-// Select elements
-const slider = document.getElementById('slider');
-const nextButton = document.getElementById('nextBtn');
-
-// Number of images displayed at a time
-const imagesToShow = 4;
-// Total images
-const totalImages = 10;
-
-// Current index of the first visible image
-let currentIndex = 0;
-
-nextButton.addEventListener('click', () => {
-    // Increment the index by the number of images to show
-    currentIndex += imagesToShow;
-
-    // Calculate the offset for the translation
-    const offset = currentIndex * (100 / imagesToShow); // 100% / imagesToShow for each image width
-
-    // Move the slider to the left by the calculated offset
-    slider.style.transform = `translateX(-${offset}%)`;
-
-    // Hide the Next button when reaching the end
-    if (currentIndex >= totalImages - imagesToShow) {
-        nextButton.style.display = 'none';
+new Swiper('.card-wrapper', {
+    // direction: 'vertical',
+    loop: true,
+  
+    // pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // responsive 
+  breakpoints:{
+    0:{
+        slidesPerView: 1
+    },
+    520:{
+        slidesPerView: 2
+    },
+    768:{
+        slidesPerView: 3
+    },
+    1024:{
+        slidesPerView: 4
     }
-});
+  },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
